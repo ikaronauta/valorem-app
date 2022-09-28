9a22f6245b52c3edd01e4426c349dfafc5de4493
ikaronauta <jaortiz0496@misena.edu.co>
2022-09-28 17:08:56 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./views/Home";
+import { Login } from "./views/Login";
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
+}
