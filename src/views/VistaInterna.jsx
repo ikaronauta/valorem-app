a6c4535aaf5a73cb866356485e9698403394f196
ikaronauta <81399721+ikaronauta@users.noreply.github.com>
2022-10-12 16:08:19 -0500

    update

--- src/views/VistaInterna.jsx
+++ src/views/VistaInterna.jsx
@@ -5,40 +5,44 @@ import styles from "../css/components/MenuInterno.module.css";
 export function VistaInterna({ datos }) {
   const [datosInternos, setDatosInternos] = useState(datos[0].datos);
 
-  function prueba(){
-    document.getElementById("prueba").style.left = "-100%";
+  function ocultarMostrar() {
+    console.log(document.getElementById("prueba").style.left);
+    if (document.getElementById("prueba").style.left === "-100%")
+      document.getElementById("prueba").style.left = "0%";
+    else document.getElementById("prueba").style.left = "-100%";
   }
 
   return (
     <>
-    <header>
-      <div>
-        <label onClick={prueba}>
-          <ion-icon name="menu" />
-        </label>
-      </div>
-      <nav id="prueba" className={styles.menuInterno}>
-        {datos.map((dato) => {
-          return (
-            <>
-              <div
-                key={dato.id}
-                className={styles.containerInterno}
-                onClick={function () {
-                  setDatosInternos(dato.datos);
-                }}
-              >
-                <div className={styles.containerIcon}>
-                  <ion-icon name={dato.icon}></ion-icon>
+      <header>
+        <div>
+          <label onClick={ocultarMostrar}>
+            <ion-icon name="menu" />
+          </label>
+        </div>
+        <nav id="prueba" className={styles.menuInterno}>
+          {datos.map((dato) => {
+            return (
+              <>
+                <div
+                  key={dato.id}
+                  className={styles.containerInterno}
+                  onClick={function () {
+                    setDatosInternos(dato.datos);
+                    ocultarMostrar();
+                  }}
+                >
+                  <div className={styles.containerIcon}>
+                    <ion-icon name={dato.icon}></ion-icon>
+                  </div>
+                  <h3>{dato.title}</h3>
                 </div>
-                <h3>{dato.title}</h3>
-              </div>
-            </>
-          );
-        })}
-      </nav>
-    </header>
-    <ContenidoInterno datos={datosInternos} />
+              </>
+            );
+          })}
+        </nav>
+      </header>
+      <ContenidoInterno datos={datosInternos} />
     </>
   );
 }
