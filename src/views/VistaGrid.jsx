eab1a5527a931f0dc377279b0a107e6e53bdd6e8
ikaronauta <81399721+ikaronauta@users.noreply.github.com>
2022-10-21 18:06:53 -0500

    update

--- src/views/VistaGrid.jsx
+++ src/views/VistaGrid.jsx
@@ -3,9 +3,7 @@ import { Grid } from "../components/Grid";
 //Importación Estilos
 import styles from "../css/general.module.css";
 
-export function VistaGrid({ datos, title }) {
-  if (title) document.getElementById("title").innerHTML = title;
-  else document.getElementById("title").innerHTML = "Titulo";
+export function VistaGrid({ datos }) {
   return (
     <div className={styles.outlet}>
       <Grid datos={datos} />
