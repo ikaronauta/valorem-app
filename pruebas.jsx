9915237afb821db0f3c6aa3a85fc766701101c86
ikaronauta <81399721+ikaronauta@users.noreply.github.com>
2022-10-26 09:36:56 -0500

    update logica login

--- pruebas.jsx
+++ pruebas.jsx
@@ -1,7 +1,69 @@
-const handleLogin = async () => {
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useUserSetAuth } from "../context/AuthProvider";
+import { LoginAuth } from "./LoginAuth";
+
+export function IniciarSesion(user, pass) {
+  debugger;
+  const cargarUsuario = useUserSetAuth();
+  const navigate = useNavigate();
+
+  const promesa = LoginAuth(user, pass);
+  promesa.then(resolve, reject);
+
+  function resolve(data) {
+    if (data.ID_ESTADO === "05") {
+      let fechaVencimiento = new Date(
+        data.FECHA_VENCIMIENTO
+      ).toLocaleDateString();
+      cargarUsuario(data);
+      navigate("/home");
+
+      Swal.fire({
+        title: `Bienvenido ${data.USUARIO}`,
+        text: `Fecha vencimiento clave: ${fechaVencimiento}`,
+        confirmButtonColor: "#005DC9",
+        confirmButtonText: "Siguiente",
+        icon: "success",
+      });
+    } else {
+      navigate("/");
+      Swal.fire({
+        text: "¡Datos incorrectos!",
+        confirmButtonColor: "#005DC9",
+        confirmButtonText: "Siguiente",
+        icon: "error",
+      });
+    }
+  }
+
+  function reject(data) {
+    navigate("/");
+    Swal.fire({
+      title: "!Acceso denegado!",
+      text: data.message + " - " + data.code,
+      confirmButtonColor: "#005DC9",
+      confirmButtonText: "Siguiente",
+      icon: "error",
+    });
+  }
+}
+
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useUserSetAuth } from "../context/AuthProvider";
+
+export const LoginAuth = async (user, pass) => {
+  const cargarUsuario = useUserSetAuth();
+
+  const navigate = useNavigate();
+
   const url = `https://valoremanalitica.bpmco.co/login?user=${user}&pass=${pass}`;
   const result = await axios.get(url);
   const response = JSON.parse(result.data.slice(1, -1));
+
+  cargarUsuario(response);
 
   let fechaVencimiento;
 
@@ -14,7 +76,6 @@ const handleLogin = async () => {
     });
   } else {
     if (response.hasOwnProperty("ID_ESTADO")) {
-      localStorage.setItem("VALOREM_APP_LOGGED", JSON.stringify(response));
       fechaVencimiento = new Date(
         response.FECHA_VENCIMIENTO
       ).toLocaleDateString();
