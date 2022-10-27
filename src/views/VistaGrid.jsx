81963aa681beff453a0224568e989981d5324029
ikaronauta <81399721+ikaronauta@users.noreply.github.com>
2022-10-27 15:53:46 -0500

    se agrega spinner para el home

--- src/views/VistaGrid.jsx
+++ src/views/VistaGrid.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { PulseLoader } from "react-spinners";
 import { getData } from "../auth/getData";
 import { Grid } from "../components/Grid";
 
@@ -7,15 +8,23 @@ import styles from "../css/general.module.css";
 
 export function VistaGrid({ endPoint }) {
   const [datos, setDatos] = useState([]);
+  const [loading, setloaDing] = useState(false);
 
-  const promesa = getData(endPoint);
-  promesa.then((data) => {
-    setDatos(data);
-  });
+  useEffect(() => {
+    setloaDing(true);
+    getData(endPoint).then((data) => {
+      setDatos(data);
+      setloaDing(false);
+    });
+  }, [endPoint]);
 
   return (
     <div className={styles.outlet}>
-      <Grid datos={datos} />
+      {loading ? (
+        <PulseLoader className={styles.sppiner} color="#005dc9" />
+      ) : (
+        <Grid datos={datos} />
+      )}
     </div>
   );
 }
