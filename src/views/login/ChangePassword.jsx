5567e3bce5746ce4f314d9e70e646e390c725ee7
ikaronauta <81399721+ikaronauta@users.noreply.github.com>
2022-10-27 09:42:14 -0500

    pruebas changePassword

--- src/views/login/ChangePassword.jsx
+++ src/views/login/ChangePassword.jsx
@@ -1,31 +1,89 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 import styles from "../../css/general.module.css";
 
+//https://valoremanalitica.bpmco.co/changePass?user=BPMCO_PORTAL&pass=BPMCOInicio.12345678.&newPass=BPMCOInicio-12345678.
+
 export function ChangePassword() {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const [newPass, setNewPass] = useState("");
+  const [repeatNewPass, setRepeatNewPass] = useState("");
+
+  const handleChangePassword = () => {
+    if (user === "" || pass === "" || newPass === "" || repeatNewPass === "") {
+      Swal.fire({
+        text: "¡Debe ingresar toda la información para poder continuar!",
+        confirmButtonColor: "#005DC9",
+        confirmButtonText: "Siguiente",
+        icon: "error",
+      });
+    } else if (pass === newPass) {
+      Swal.fire({
+        text: "¡La nueva contraseña debe ser diferente a la contraseña antigua!",
+        confirmButtonColor: "#005DC9",
+        confirmButtonText: "Siguiente",
+        icon: "error",
+      });
+    } else if (newPass !== repeatNewPass) {
+      Swal.fire({
+        text: "¡Las contraseñas no coinciden!",
+        confirmButtonColor: "#005DC9",
+        confirmButtonText: "Siguiente",
+        icon: "error",
+      });
+    }
+
+    // console.log(user);
+    // console.log(pass);
+    // console.log(newPass);
+    // console.log(repeatNewPass);
+    // console.log(newPass === repeatNewPass);
+  };
+
   return (
     <div className={styles.contenedor}>
       <h1>PORTAL DE SERVICIO</h1>
-      <input type="text" placeholder="Usuario" className={styles.input} />
+      <input
+        type="text"
+        placeholder="Usuario"
+        className={styles.input}
+        onChange={(e) => {
+          setUser(e.target.value);
+        }}
+      />
       <input
         type="password"
         placeholder="Contraseña Actual"
         className={styles.input}
+        onChange={(e) => {
+          setPass(e.target.value);
+        }}
       />
       <input
         type="password"
         placeholder="Contraseña Nueva"
         className={styles.input}
+        onChange={(e) => {
+          setNewPass(e.target.value);
+        }}
       />
       <input
         type="password"
         placeholder="Repetir Contraseña"
         className={styles.input}
+        onChange={(e) => {
+          setRepeatNewPass(e.target.value);
+        }}
       />
       <div className={styles.grupoBotones}>
         <Link to="/">
           <button className={styles.boton}>Volver</button>
         </Link>
-        <button className={styles.boton}>Cambiar Contraseña</button>
+        <button className={styles.boton} onClick={handleChangePassword}>
+          Cambiar Contraseña
+        </button>
       </div>
     </div>
   );
